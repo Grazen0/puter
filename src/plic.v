c61fede55cf5686a15a3b54b765a8d1e6f6bc25d
José Daniel Grayson <josedanielgrayson@proton.me>
2025-12-11 21:51:56 -0500

    feat: add plic module

---
+++ src/plic.v
@@ -0,0 +1,92 @@
+`default_nettype none `timescale 1ns / 1ps
+
+module plic_gateway (
+    input wire clk,
+    input wire rst_n,
+
+    input  wire int_signal,
+    output wire irq
+);
+  wire int_sync;
+  reg  int_sync_prev;
+
+  synchronizer synchronizer (
+      .clk  (clk),
+      .rst_n(rst_n),
+
+      .in (int_signal),
+      .out(int_sync)
+  );
+
+  always @(posedge clk) begin
+    int_sync_prev <= int_sync;
+  end
+
+  assign irq = int_sync & ~int_sync_prev;  // posedge
+endmodule
+
+module plic #(
+    parameter PORTS = 4,
+    parameter ID_WIDTH = $clog2(PORTS),
+    parameter PRIORITY_WIDTH = $clog2(PORTS + 1)
+) (
+    input wire clk,
+    input wire rst_n,
+
+    input wire [PORTS-1:0] int_signal,
+    input wire [PORTS-1:0] int_claim,
+
+    output wire out_int_pending,
+    output wire [ID_WIDTH-1:0] out_int_id
+);
+  wire [PORTS-1:0] irq;
+
+  generate
+    genvar i;
+    for (i = 0; i < PORTS; i = i + 1) begin : g_gateway
+      plic_gateway gateway (
+          .clk  (clk),
+          .rst_n(rst_n),
+
+          .int_signal(int_signal[i]),
+          .irq       (irq[i])
+      );
+    end
+  endgenerate
+
+  reg [PORTS-1:0] int_pending;
+  reg [PORTS-1:0] int_enable;
+  reg [PORTS-1:0][PRIORITY_WIDTH-1:0] int_priority;
+
+  integer i2;
+
+  always @(posedge clk) begin
+    if (!rst_n) begin
+      int_pending <= {PORTS{1'b0}};
+      int_enable  <= {PORTS{1'b1}};  // TODO: set to 0 on reset
+    end else begin
+      for (i2 = 0; i2 < PORTS; i2 = i2 + 1) begin
+        if (irq[i2]) int_pending[i2] <= 1;
+        else if (int_claim[i2]) int_pending[i2] <= 0;
+      end
+    end
+  end
+
+  wire [PORTS:0][PRIORITY_WIDTH-1:0] max_priority;
+  wire [PORTS:0][ID_WIDTH-1:0] max_id;
+
+  assign max_priority[0] = 0;
+  assign max_id[0]       = 0;
+
+  generate
+    for (i = 0; i < PORTS; i = i + 1) begin : g_priority_logic
+      wire int_cond = int_enable[i] && int_pending[i] && int_priority[i] > max_priority[i];
+
+      assign max_priority[i+1] = int_cond ? int_priority[i] : max_priority[i];
+      assign max_id[i+1]       = int_cond ? i : max_id[i];
+    end
+  endgenerate
+
+  assign out_int_pending = max_priority[PORTS] != 0;
+  assign out_int_id = max_id[PORTS];
+endmodule
--- src/synchronizer.v
+++ src/synchronizer.v
@@ -2,21 +2,29 @@
 
 module synchronizer #(
     parameter STAGES = 2,
-    parameter WIDTH  = 1
+    parameter RESET_VALUE = 1'b0
 ) (
-    input  wire             clk,
-    input  wire [WIDTH-1:0] in,
-    output wire [WIDTH-1:0] out
+    input wire clk,
+    input wire rst_n,
+
+    input  wire in,
+    output wire out
 );
-  reg [WIDTH-1:0] stages[0:STAGES-1];
+  reg [STAGES-1:0] stages;
 
   integer i;
 
   always @(posedge clk) begin
-    stages[0] <= in;
+    if (!rst_n) begin
+      for (i = 0; i < STAGES; i = i + 1) begin
+        stages[i] <= RESET_VALUE;
+      end
+    end else begin
+      stages[0] <= in;
 
-    for (i = 1; i < STAGES; i = i + 1) begin
-      stages[i] <= stages[i-1];
+      for (i = 1; i < STAGES; i = i + 1) begin
+        stages[i] <= stages[i-1];
+      end
     end
   end
 
