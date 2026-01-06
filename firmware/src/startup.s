c20a236977b2308c0f6e1bddc1778870da245613
José Daniel Grayson <josedanielgrayson@proton.me>
2026-01-06 12:53:45 -0500

    perf: optimize data and bss init

--- firmware/src/startup.s
+++ firmware/src/startup.s
@@ -13,25 +13,19 @@ _start:
     la      t1, _data_end
     la      t2, _text_end
 init_data:
-    bgeu    t0, t1, data_done
-
     lw      t3, 0(t2)
     addi    t2, t2, 4 # reordered to prevent lw stall
     sw      t3, 0(t0)
     addi    t0, t0, 4
-
-    j       init_data
-data_done:
+    blt     t0, t1, init_data
 
     # Initialize .bss
     la      t0, _bss_start
     la      t1, _bss_end
 clear_bss:
-    bgeu    t0, t1, bss_done
     sw      zero, 0(t0)
     addi    t0, t0, 4
-    j       clear_bss
-bss_done:
+    blt     t0, t1, clear_bss
 
     call    __libc_init_array
     call    main
