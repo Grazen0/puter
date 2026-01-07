f1f48c7899e459fa6dcb7a8699daf7f3ff03455d
José Daniel Grayson <josedanielgrayson@proton.me>
2026-01-07 00:06:09 -0500

    Revert "perf: optimize data and bss init"
    This reverts commit c20a236977b2308c0f6e1bddc1778870da245613.

--- firmware/src/startup.s
+++ firmware/src/startup.s
@@ -13,19 +13,25 @@ _start:
     la      t1, _data_end
     la      t2, _text_end
 init_data:
+    bgeu    t0, t1, data_done
+
     lw      t3, 0(t2)
     addi    t2, t2, 4 # reordered to prevent lw stall
     sw      t3, 0(t0)
     addi    t0, t0, 4
-    blt     t0, t1, init_data
+
+    j       init_data
+data_done:
 
     # Initialize .bss
     la      t0, _bss_start
     la      t1, _bss_end
 clear_bss:
+    bgeu    t0, t1, bss_done
     sw      zero, 0(t0)
     addi    t0, t0, 4
-    blt     t0, t1, clear_bss
+    j       clear_bss
+bss_done:
 
     call    __libc_init_array
     call    main
